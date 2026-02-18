276a01d356e94da9c51db351ff617ccb3b660704
SehajAnimator <90809787+SehajAnimator@users.noreply.github.com>
2026-02-18 10:19:13 +0530

    updated source files

--- Game/Source/Game/Private/cPlayer.cpp
+++ Game/Source/Game/Private/cPlayer.cpp
@@ -2,6 +2,8 @@
 
 
 #include "cPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/Engine.h"
 
 // Sets default values
 AcPlayer::AcPlayer()
@@ -9,6 +11,15 @@ AcPlayer::AcPlayer()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Constructor Manual Gen
+		// Root component
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+		// Camera
+	defCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("DefaultCamera"));
+	defCamera->SetupAttachment(RootComponent);
+	defCamera->SetRelativeLocation(FVector(0.f, 0.f, 50.f));
+
+	GAreScreenMessagesEnabled = true;	
 }
 
 // Called when the game starts or when spawned
@@ -23,6 +34,13 @@ void AcPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Update Loop
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Test"));
+	}
+
+	UE_LOG(LogTemp, Error, TEXT("tick working"));
 }
 
 // Called to bind functionality to input
--- Game/Source/Game/Public/cPlayer.h
+++ Game/Source/Game/Public/cPlayer.h
@@ -19,6 +19,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Custom variables
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* defCamera;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
