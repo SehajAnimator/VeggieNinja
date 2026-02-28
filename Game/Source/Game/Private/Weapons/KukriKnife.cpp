bfc24589762e36290c00c78fdef4ea362fb20a46
SehajAnimator <90809787+SehajAnimator@users.noreply.github.com>
2026-02-28 18:35:56 +0530

    Added KukriKnife Template

---
+++ Game/Source/Game/Private/Weapons/KukriKnife.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2026 Studios SehajAvastha
+// 
+// This file is part of VeggieNinja.
+// 
+// Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+// You may not use this file except in compliance with the License.
+// 
+// You are free to share and adapt this work for academic and research purposes only,
+// provided that proper attribution is given and derivatives are licensed under the same terms.
+// Commercial use is strictly prohibited.
+// 
+// Full license text: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+
+#include "Weapons/KukriKnife.h"
+
+AKukriKnife::AKukriKnife()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	baseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+}
